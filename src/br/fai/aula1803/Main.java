1d1ef756f275bd21a099c0f8b393d2c3203636ab
fernandinho <lfrfernandinho@gmail.com>
2020-03-18 23:54:07 -0300

    terceiro commit

--- src/br/fai/aula1803/Main.java
+++ src/br/fai/aula1803/Main.java
@@ -11,7 +11,19 @@ public class Main {
 
 	private void start() {
 		// TODO Auto-generated method stub
+        int opcao = 0;
 		
+		do {
+			System.out.println("1. Cadastrar cavalo");
+			System.out.println("2. Cadastrar girafa");
+			System.out.println("3. Listar cavalos");
+			System.out.println("4. Listar girafas0");
+			System.out.println("5. Listar todos os animais");
+			System.out.println("6. remover umanimal por id");
+			System.out.println("7. Exibir a quantidade de animais cadastrados por tipo");
+			System.out.println("8. Sair");
+			
+		}while(opcao == 8);
 		
 	}
 
