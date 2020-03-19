06597f66a797eb656ffa90b7b5b10b516da33a16
fernandinho <lfrfernandinho@gmail.com>
2020-03-18 23:43:32 -0300

    primeiro commit

---
+++ src/br/fai/aula1803/Main.java
@@ -0,0 +1,18 @@
+package br.fai.aula1803;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
